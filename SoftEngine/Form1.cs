f9570534b57b164b25f9c713e0f0995cc1764161
永生 王 <705346166@qq.com>
2021-01-30 20:02:23 +0800

    monkey  mesh

--- SoftEngine/Form1.cs
+++ SoftEngine/Form1.cs
@@ -16,7 +16,7 @@ namespace SoftEngine
         private Stopwatch stopwatch = new Stopwatch();
         private long runTIme;
         private Device device;
-        Mesh mesh = new Mesh("Cube", 8, 12);
+        Mesh[] meshes;
         Camera mera = new Camera();
 
         public Form1()
@@ -29,31 +29,8 @@ namespace SoftEngine
             Bitmap bmp = new Bitmap(640, 480);
 
             device = new Device(bmp);
-
-            mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            mesh.Vertices[1] = new Vector3(1, 1, 1);
-            mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            mesh.Vertices[3] = new Vector3(1, -1, 1);
-            mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            mesh.Vertices[5] = new Vector3(1, 1, -1);
-            mesh.Vertices[6] = new Vector3(1, -1, -1);
-            mesh.Vertices[7] = new Vector3(-1, -1, -1);
-
-            mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
-            mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
-            mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
-            mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
-            mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
-
-            mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
-            mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
-            mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
-            mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
-            mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
-
-            mera.Position = new Vector3(0, 0, 0.05f);
+            meshes = device.LoadJSONFileAsync("Json\\monkey.txt");
+            mera.Position = new Vector3(0, 0, 0.02f);
             mera.Target = Vector3.Zero;
             stopwatch.Start();
 
@@ -90,11 +67,14 @@ namespace SoftEngine
 
             device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            foreach (var mesh in meshes)
+            {
+                // rotating slightly the meshes during each frame rendered
+                mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
 
-            //// Doing the various matrix operations
-            device.Render(mera, mesh);
+            // Doing the various matrix operations
+            device.Render(mera, meshes);
             // Flushing the back buffer into the front buffer
             device.Present(grfc);
             grfc.DrawString((stopwatch.ElapsedMilliseconds - runTIme).ToString(), new Font("宋体", 28), new SolidBrush(Color.Blue), new PointF(10, 10));
